c0ec49ed618ef29732d3e1eb46b4416562ed14f0
HauSianCing <cc.pearl.94@gmail.com>
2023-08-22 13:40:26 +0630

    added project photo

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,7 @@
 import "./Projects.css";
+import emp_prj from "../assets/emp_reg.png"
+import quiz_prj from "../assets/quiz.png"
+import  weather_app_prj from "../assets/weather.png"
 export const Projects = () => {
     return (
         <div className="Projects">
@@ -7,21 +10,21 @@ export const Projects = () => {
                 Creative Projects
             </h1>
             <div className="project">
-            <Project />
-            <Project />
-            <Project />
+            <Project image={emp_prj} description={"Employee Resgistration"}/>
+            <Project image={quiz_prj} description={"Quiz App"}/>
+            <Project image={weather_app_prj} description={"Weather App"}/>
             </div>
            
         </div>
     );
 }
-const Project = () => {
+const Project = (props) => {
     return (
         <div className="projectCard">
-            <img src="/IT-image.jpg" className="project-image"/>
+            <img src={props.image} className="project-image"/>
             <div className="prj-description">
             <p>
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur
+            {props.description}
             </p>
             </div>
         </div>
